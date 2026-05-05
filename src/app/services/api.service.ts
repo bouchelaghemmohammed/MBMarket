209420400e310d4c06b6fee04a3208004be173fc
MOH <bouchelaghemmohammed@gmail.com>
2026-05-04 20:10:28 -0400

    add environment prod !

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,14 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = 'http://localhost:5000/api';
+  private apiUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient, private authService: AuthService) {}
+  constructor(private http: HttpClient, private authService: AuthService) { }
 
   private getHeaders() {
     const token = this.authService.getToken();
--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { io, Socket } from 'socket.io-client';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SocketService {
   private socket: Socket;
-  private readonly url: string = 'http://localhost:5000'; // Should be from environment in prod
+  private readonly url: string = environment.socketUrl;
 
   constructor() {
     this.socket = io(this.url);
