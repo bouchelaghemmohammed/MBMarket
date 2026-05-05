a758ac4541d5ba9544dd448e42d0cb3bc2935f68
MOH <bouchelaghemmohammed@gmail.com>
2026-05-04 20:15:53 -0400

    add environment prod 4 !

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://mbmarketbackend.onrender.com',
-  socketUrl: 'https://mbmarketbackend.onrender.com'
+  apiUrl: 'https://mbmarketbackend.onrender.com/api',
+  socketUrl: 'https://mbmarketbackend.onrender.com',
+
 };
